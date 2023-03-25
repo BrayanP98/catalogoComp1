2bf2b8a54c084a97cea4fd76a0ff00b66948b2b9
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-03-25 02:36:39 -0500

    add ajax file

--- ajax.js
+++ ajax.js
@@ -31,6 +31,9 @@
                     var seccion_hogar= document.getElementById("hogar");
                     var seccion_papeleria= document.getElementById("papeleria");
                     var seccion_ferreteria= document.getElementById("ferreteria");
+                    var seccion_varios= document.getElementById("varios");
+                    var btn_whatsap= document.getElementById("whatsapp");
+                    
                    // var hoja= document.getElementById("hogar");
                     var cardProd = document.createElement("div");
                     cardProd.id="producto";
@@ -50,13 +53,13 @@
                         var p = document.createElement("p");
                         var valor = document.createElement("a");
                         var buton = document.createElement("button");
-                        buton.id="whatsapp";
+                        buton.id="revisar";
                         buton.setAttribute('target',"_blank");
                         valor.id="precio"
                         var pText = document.createTextNode(item.nombre);
                         valor.innerHTML= item.valor
-                       // buton.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
-                        buton.innerHTML="Whatsapp"
+                        
+                        buton.innerHTML="Revisar"
                         p.appendChild(pText);
                         descrip.appendChild(p);
                         descrip.appendChild(valor);
@@ -68,7 +71,7 @@
                             document.querySelector("#pop-up").classList.add("active")
 
                             document.querySelector("#image_prod").setAttribute('src',"imagenes_competidor/"+item.nombre+".png")
-                           esconder()
+                            btn_whatsap.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
                     }
                     
                         seccion_hogar.appendChild(cardProd);
@@ -83,13 +86,13 @@
                         var p = document.createElement("p");
                         var valor = document.createElement("a");
                         var buton = document.createElement("button");
-                        buton.id="whatsapp";
+                        buton.id="revisar";
                         buton.setAttribute('target',"_blank");
                         valor.id="precio"
                         var pText = document.createTextNode(item.nombre);
                         valor.innerHTML= item.valor
-                     //   buton.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
-                        buton.innerHTML="Whatsapp"
+                        
+                        buton.innerHTML="Revisar"
                         p.appendChild(pText);
                         descrip.appendChild(p);
                         descrip.appendChild(valor);
@@ -101,6 +104,7 @@
                             document.querySelector("#pop-up").classList.add("active")
 
                             document.querySelector("#image_prod").setAttribute('src',"imagenes_competidor/"+item.nombre+".png")
+                            btn_whatsap.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
                     }
                     
                         seccion_papeleria.appendChild(cardProd);
@@ -115,13 +119,13 @@
                         var p = document.createElement("p");
                         var valor = document.createElement("a");
                         var buton = document.createElement("button");
-                        buton.id="whatsapp";
+                        buton.id="revisar";
                         buton.setAttribute('target',"_blank");
                         valor.id="precio"
                         var pText = document.createTextNode(item.nombre);
                         valor.innerHTML= item.valor
-                        //buton.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
-                        buton.innerHTML="Whatsapp"
+                       //buton.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
+                        buton.innerHTML="Revisar"
                         p.appendChild(pText);
                         descrip.appendChild(p);
                         descrip.appendChild(valor);
@@ -134,9 +138,43 @@
                                 document.querySelector("#pop-up").classList.toggle("active")
 
                                 document.querySelector("#image_prod").setAttribute('src',"imagenes_competidor/"+item.nombre+".png")
-
+                                btn_whatsap.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
                         }
                         seccion_ferreteria.appendChild(cardProd);
+                    }
+
+                    if(item.categoria=="varios" ){
+
+                        imagen.setAttribute('src',"imagenes_competidor/"+item.nombre+".png");
+                        
+                    
+                        var descrip = document.createElement("div");
+                        descrip.id="descrip"
+                        var p = document.createElement("p");
+                        var valor = document.createElement("a");
+                        var buton = document.createElement("button");
+                        buton.id="revisar";
+                        buton.setAttribute('target',"_blank");
+                        valor.id="precio"
+                        var pText = document.createTextNode(item.nombre);
+                        valor.innerHTML= item.valor
+                       //buton.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
+                        buton.innerHTML="Revisar"
+                        p.appendChild(pText);
+                        descrip.appendChild(p);
+                        descrip.appendChild(valor);
+                        descrip.appendChild(buton); 
+                        cardProd.appendChild(imagen);
+                        cardProd.appendChild(descrip);
+                        
+                        buton.onclick= function(){
+
+                                document.querySelector("#pop-up").classList.toggle("active")
+
+                                document.querySelector("#image_prod").setAttribute('src',"imagenes_competidor/"+item.nombre+".png")
+                                btn_whatsap.setAttribute('href', "https://wa.me/573026055289?text= Hola buen dia, Quiesiera hecer un pedido de"+item.nombre+"🙏")
+                        }
+                        seccion_varios.appendChild(cardProd);
                     }
                        
                     // var nom_prod="producto"+i;
